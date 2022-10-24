f858d2b17dfe20128f5fb2b8ebcd929455d9b191
mizok <karasunohina@gmail.com>
2022-10-24 21:04:24 +0800

    Updates

--- src/ts/util/service.d.ts
+++ src/ts/util/service.d.ts
@@ -1,3 +1,4 @@
+export declare const pointerEvent: string;
 export declare function doConfirm(template: string): Promise<unknown>;
 export declare function doAlert(template: string): void;
 export declare function searchMusic(): Promise<false | string>;
